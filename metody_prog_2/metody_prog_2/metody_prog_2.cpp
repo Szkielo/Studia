700f3660354c161453d87b9ccbe9afab276e0de8
Szkielo <45492449+Szkielo@users.noreply.github.com>
2020-12-18 18:31:20 +0100

    Add files via upload

---
+++ metody_prog_2/metody_prog_2/metody_prog_2.cpp
@@ -0,0 +1,70 @@
+﻿// input: liczba n
+// output: wygenerowane zbiory w kolejności leksykalnej
+// generowanie: start na 1111 dalej 1112 itp, ostatnia cyfra ma być maksymalnie o 1 wieksza od poprzedniej
+// wypisanie -> sprawdzenie warunku -> true: "przeskok na następną wartość" false: (ostatnia komórka)++
+// wszystko wykonuje się w petli przerwanej gdy counter=n-1 czyli wszystkie wartości spełnią warunek n=[n-1]+1
+
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void wypisz(int * tab, int rozmiar, ofstream& zapis) {
+	for (int i = 0; i < rozmiar; i++) {
+		cout << tab[i] << " ";
+		zapis << tab[i] << " ";
+	}
+	cout << endl;
+	zapis << endl;
+}
+
+void check(int* tab, int indeks, int rozmiar, int &swap, int &end, ofstream& zapis) { // indeks = rozmiar-1
+	if (end == 1) {}
+	else if (indeks - 1 < 0) end = 1;  // warunek konczacy petle (kiedy dotrze do zerowego elementu tablicy)
+	else if ((tab[indeks]) == (tab[indeks - 1] + 1)) { //jezeli tab[n] bedzie o jeden wiekszy od tab[n-1], sprawdz poprzedni element
+		swap = 1;
+		check(tab, indeks - 1, rozmiar, swap, end, zapis);
+	}
+	else if (swap == 1) { //jezeli swap == 1 (z rekurencji poprzedni el nie jest mniejszy o 1)
+		tab[indeks]++;
+		for (int i = indeks + 1; i < rozmiar; i++) {
+			tab[i] = 1;
+		}
+		swap = 0;
+		wypisz(tab, rozmiar, zapis);
+	}
+	else {
+		tab[indeks]++;
+		wypisz(tab, rozmiar, zapis);
+	}
+}
+
+int main()
+{
+	int n;
+	cout << "\nProgram dokonujacy co najwyzej n-blokowych podzialow zbiorow. Zbiory sa wypisane na ekran oraz do pliku podzialy.txt, w kolejnosci leksykokgraficznej.";
+	cout << "\nWprowadz liczbe elementow zbioru: ";
+	cin >> n;
+
+	int * tab = new int[n]; // dane przechowywane w tablicy
+	for (int i = 0; i < n; i++) {
+		tab[i] = 1;
+	}
+	ofstream zapis("podzialy.txt");
+	int end = 0;
+	int swap = 0;
+	wypisz(tab, n, zapis);
+	do {
+		check(tab, n - 1, n, swap, end, zapis); //(tablica, sprawdzany indeks, rozmiar tablicy, zmienna konczaca petle, plik do zapisu)
+	} while (end != 1);
+
+	zapis.close();
+	delete[] tab;
+	char pause;
+	cin >> pause;
+	return 0;
+}
+
+
---
+++ metody_prog_3/metody_prog_3/metody_prog_3.cpp
@@ -0,0 +1,100 @@
+﻿// Drzewa t-narne
+//
+
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+void wypisz(int * Z, int* X, int link, int wezly, ofstream& zapisX, ofstream& zapisZ) {
+	cout << "";
+	for (int i = 0; i < link; i++) {
+		cout << Z[i] << " ";
+		zapisZ << Z[i] << " ";
+	}
+	cout << "                               ";
+	for (int i = 0; i < wezly; i++) {
+		cout << X[i];
+		zapisX << X[i];
+	}
+	cout << endl;
+	zapisZ << endl;
+	zapisX << endl;
+}
+
+void resetZ(int* Z, int link, int kids, int wezly, int& step, bool& end) {
+	if (link - 1 == 0) end = true;
+	else if (Z[link - 1] == (wezly - step)) {
+		step += kids;
+		resetZ(Z, link - 1, kids, wezly, step, end);
+		Z[link - 1] = Z[link - 2] + 1;
+	}
+	else {
+		Z[link - 1]++;
+	}
+}
+
+void genX(int* Z, int * X, int link, int wezly) {
+	for (int i = 0; i < wezly; i++) {
+		X[i] = 0;
+	}
+	for (int i = 0; i < link; i++) {
+		X[Z[i] - 1] = 1;
+	}
+}
+
+void generate(int * Z, int* X, int link, int kids, int wezly, ofstream& zapisX, ofstream& zapisZ, int& gencount) { // go up to slots from poprzedni el + 1, then add 1 to one before, go again... maksymalnie (wierzcholki) razy
+	int step = 0;
+	bool end = false;
+	do {
+			for (int i = Z[link - 1]; i <= wezly; i++) {
+				Z[link - 1] = i;
+				genX(Z, X, link, wezly);
+				wypisz(Z, X, link, wezly, zapisX, zapisZ);
+				gencount++;
+			}
+			resetZ(Z, link, kids, wezly, step, end); // petla przeszla, trzeba dodać do poprzedniego i zresetować
+			step = 0;
+	} while (end == false);
+}
+
+
+int main()
+{
+	cout << "Program generujacy Z-sekwencje i X-sekwencje dla drzew t-narnych.";
+	cout << "\nWyniki zostana zapisane do dwoch roznych plikow.";
+	cout << "\nPodaj liczbe potomkow na wierzcholek: ";
+	int link, kids;
+	cin >> kids;
+	cout << "\nPodaj liczbe wierzcholkow obdarzonych potomkami: ";
+	cin >> link;
+
+	int wezly = (link - 1) * kids + 1; // pomijamy niemajace potomkow, +1 z korzenia
+	int * X = new int[wezly] {};
+	int * Z = new int[link] {};
+	for (int i = 0; i < link; i++) {
+		Z[i] = i + 1;
+	}
+	int gencount = 0;
+
+	ofstream zapisX("listaX.txt");
+	ofstream zapisZ("listaZ.txt");
+	//---------
+	cout << endl;
+	cout << "\nZ-sekwencje:                       X-sekwencje:" << endl;
+	generate(Z, X, link, kids, wezly, zapisX, zapisZ, gencount);
+	//---------
+	cout << "\nLiczba wygenerowanych sekwencji: " << gencount;
+	zapisZ << "\nLiczba wygenerowanych sekwencji: " << gencount;
+	zapisX << "\nLiczba wygenerowanych sekwencji: " << gencount;
+	zapisX.close();
+	zapisZ.close();
+	delete[]X;
+	delete[]Z;
+	char pause;
+	cin >> pause;
+	return 0;
+}
+
